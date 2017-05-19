dfd24e91d2f73a2de8fc7f35ff751eb2bb13b12f
Andrew Freiday <andrewfreiday@gmail.com>
2017-05-19 13:50:21 -0400

    Update vimrc, add more plugins

--- vim/setup.sh
+++ vim/setup.sh
@@ -4,11 +4,36 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cd ~/.vim/bundle
 
+git clone https://github.com/bkad/CamelCaseMotion.git
+git clone https://github.com/lilydjwg/colorizer.git
+git clone https://github.com/rhysd/committia.vim
 git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/martinda/Jenkinsfile-vim-syntax.git
+git clone https://github.com/itchyny/lightline.vim.git
 git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/joshdick/onedark.vim.git
+git clone https://github.com/unblevable/quick-scope.git
 git clone https://github.com/vim-syntastic/syntastic.git
 git clone https://github.com/majutsushi/tagbar.git
-git clone https://github.com/vim-airline/vim-airline.git
+git clone https://github.com/wellle/targets.vim.git
+git clone https://github.com/leafgarland/typescript-vim.git
+git clone https://github.com/mbbill/undotree.git
+git clone https://github.com/moll/vim-bbye.git
+git clone https://github.com/MattesGroeger/vim-bookmarks.git
+git clone https://github.com/flazz/vim-colorschemes.git
+git clone https://github.com/xolox/vim-colorscheme-switcher.git
 git clone https://github.com/tpope/vim-commentary.git
 git clone https://github.com/easymotion/vim-easymotion.git
+git clone https://github.com/xolox/vim-easytags.git
+git clone https://github.com/eugen0329/vim-esearch.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/airblade/vim-gitgutter.git
+git clone https://github.com/ryanss/vim-hackernews.git
+git clone https://github.com/nathanaelkane/vim-indent-guides.git
+git clone https://github.com/xolox/vim-misc.git
+git clone https://github.com/mhinz/vim-signify.git
+git clone https://github.com/justinmk/vim-sneak.git
+git clone https://github.com/mhinz/vim-startify.git
 git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/bronson/vim-trailing-whitespace.git
+git clone https://github.com/wesQ3/vim-windowswap.vim
